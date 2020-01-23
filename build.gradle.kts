eba4300353637976bb9f2742275773eecdb4db26
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-01-23 12:24:46 +0100

    removed spring boot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.61"
     id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
     id("org.sonarqube") version "2.8"
-    id("org.springframework.boot") version "2.2.4.RELEASE"
     id("org.asciidoctor.convert") version "2.3.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.2.0"
@@ -16,15 +15,17 @@ plugins {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.3.3")
     implementation("com.fkorotkov:kubernetes-dsl:3.0")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-webflux:2.2.4.RELEASE")
     implementation("io.projectreactor.addons:reactor-extra:3.3.2.RELEASE")
 
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.3")
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.2.4.RELEASE")
     testImplementation("io.mockk:mockk:1.9.3")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.19")
     testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.0.4")
