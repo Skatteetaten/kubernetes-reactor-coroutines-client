293bb43fbfbac569903be855b8f3a9007ebe3ee0
Jarle Hansen <jarle@jarlehansen.net>
2021-02-10 15:10:20 +0100

    AOS-5447 (#28)
    * added extension functions to get/check for error messages in TokenReview
    
    * updated dependencies
    
    * read config with multiple usernames/tokens
    
    * disable token config tests on jenkins

--- src/test/kotlin/no/skatteetaten/aurora/kubernetes/KubernetesClientConfigTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/kubernetes/KubernetesClientConfigTest.kt
@@ -5,8 +5,10 @@ import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
+import no.skatteetaten.aurora.kubernetes.testutils.DisableIfJenkins
 import no.skatteetaten.aurora.kubernetes.testutils.KUBERNETES_URL
 import org.junit.jupiter.api.Test
+import org.junitpioneer.jupiter.SetSystemProperty
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
@@ -25,6 +27,20 @@ class TestConfig {
             RetryConfiguration(times = 0),
             HttpClientTimeoutConfiguration(),
             tokenLocation = "src/test/resources/test-token.txt"
+        )
+}
+
+@TestConfiguration
+class TestKubeConfig {
+    @Bean
+    fun webClientBuilder() = WebClient.builder()
+
+    @Bean
+    fun kubeConfig() =
+        KubnernetesClientConfiguration(
+            KUBERNETES_URL,
+            RetryConfiguration(times = 0),
+            HttpClientTimeoutConfiguration()
         )
 }
 
@@ -67,5 +83,37 @@ class UserTokenConfigTest {
 
         assertThat(client).isNotNull()
         assertThat(client.tokenFetcher.token()).isEqualTo("123abc")
+    }
+}
+
+@DisableIfJenkins
+@SetSystemProperty(key = "user.home", value = "src/test/resources/test-config/single")
+@SpringBootTest(classes = [TestKubeConfig::class, KubernetesClientConfig::class])
+class SingleUserConfigTest {
+
+    @TargetClient(ClientTypes.SERVICE_ACCOUNT)
+    @Autowired
+    private lateinit var client: KubernetesReactorClient
+
+    @Test
+    fun `Load single user config`() {
+        val token = client.tokenFetcher.token()
+        assertThat(token).isEqualTo("test-token")
+    }
+}
+
+@DisableIfJenkins
+@SetSystemProperty(key = "user.home", value = "src/test/resources/test-config/multiple")
+@SpringBootTest(classes = [TestKubeConfig::class, KubernetesClientConfig::class])
+class MultipleUsersConfigTest {
+
+    @TargetClient(ClientTypes.SERVICE_ACCOUNT)
+    @Autowired
+    private lateinit var client: KubernetesReactorClient
+
+    @Test
+    fun `Load single user config`() {
+        val token = client.tokenFetcher.token()
+        assertThat(token).isEqualTo("test-token")
     }
 }
