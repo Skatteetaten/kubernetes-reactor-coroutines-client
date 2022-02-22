05192ce0861250fb1c805a2eb2ab8910c9f55109
Jarle Hansen <jarle@jarlehansen.net>
2022-02-22 10:01:06 +0100

    set SO_KEEPALIVE config (#38)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java-library")
     id("idea")
-    id("no.skatteetaten.gradle.aurora") version("4.4.10")
+    id("no.skatteetaten.gradle.aurora") version("4.4.12")
 }
 
 dependencies {
@@ -11,10 +11,10 @@ dependencies {
     api("com.fkorotkov:kubernetes-dsl:2.8.1")
     api("org.springframework.boot:spring-boot-starter-webflux:2.6.3")
     api("io.projectreactor.addons:reactor-extra:3.4.6")
-    api("io.projectreactor:reactor-core:3.4.14")
+    api("io.projectreactor:reactor-core:3.4.15")
     api("io.github.microutils:kotlin-logging-jvm:2.1.21")
 
-    testImplementation("org.junit-pioneer:junit-pioneer:1.5.0")
+    testImplementation("org.junit-pioneer:junit-pioneer:1.6.0")
     testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.2.0")
     testImplementation("io.mockk:mockk:1.12.2")
     testImplementation("com.ninja-squad:springmockk:3.1.0")
