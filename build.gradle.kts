11779b28969fa1572c4159a151196e02c3164488
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2022-11-03 13:43:18 +0100

    AOS-7195 Bumping and setting java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
     id("java-library")
     id("idea")
-    id("no.skatteetaten.gradle.aurora") version("4.5.4")
+    id("no.skatteetaten.gradle.aurora") version("4.5.9")
 }
 
 dependencies {
-    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.6.4")
     api("com.fkorotkov:kubernetes-dsl:2.8.1")
     api("org.springframework.boot:spring-boot-starter-webflux")
-    api("io.github.microutils:kotlin-logging-jvm:2.1.23")
+    api("io.github.microutils:kotlin-logging-jvm:3.0.3")
 
     testImplementation("org.junit-pioneer:junit-pioneer:1.6.2")
     testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.3.1")
-    testImplementation("io.mockk:mockk:1.12.5")
+    testImplementation("io.mockk:mockk:1.13.2")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
 }
 
@@ -30,4 +30,11 @@ aurora {
 
 java {
     withSourcesJar()
+}
+repositories {
+    mavenCentral()
+}
+
+tasks.compileJava {
+    options.release.set(8)
 }
